7f88683bb309dab28243afe5d4b900dbe7385df5
zunami <hamza@isotoxin.com>
2023-01-08 23:06:45 +0100

    feat(recipe): add get by recipeID

--- api/recipe/recipe.go
+++ api/recipe/recipe.go
@@ -1,7 +1,26 @@
 package recipe
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"juro-go/database"
+	"juro-go/models"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func getRecipe(ctx *fiber.Ctx) error {
 	return ctx.SendString("Recipe")
+}
+
+func getRecipeById(ctx *fiber.Ctx) error {
+	recipeId := ctx.Params("id")
+
+	var recipe models.Recipe
+
+	result := database.DB.Where("id = ?", recipeId).First(&recipe)
+
+	if result.RowsAffected == 0 {
+		return ctx.SendStatus(404)
+	}
+
+	return ctx.JSON(&recipe)
 }
