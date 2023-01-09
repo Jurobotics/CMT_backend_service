8fa77880fe91bdbd864ba411610136cbad8c440f
zunami <hamza@isotoxin.com>
2023-01-09 16:59:58 +0100

    feat(responses): add uniform responses

---
+++ utils/messages.go
@@ -0,0 +1,23 @@
+package utils
+
+type Response struct {
+	Status bool   `json:"status"`
+	Data   any    `json:"data"`
+	Error  string `json:"error"`
+}
+
+func SuccessResponse(data any) Response {
+	return Response{
+		Status: true,
+		Data:   data,
+		Error:  "",
+	}
+}
+
+func ErrorResponse(err error) Response {
+	return Response{
+		Status: false,
+		Data:   "",
+		Error:  err.Error(),
+	}
+}
